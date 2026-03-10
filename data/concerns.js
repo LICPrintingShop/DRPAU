95c1d3a272a8360540dfe6f4691196b412fc186d
LICPrintingShop <miboeri30@gmail.com>
2026-03-10 15:59:47 +0800

    Create concerns.js

---
+++ data/concerns.js
@@ -0,0 +1,9 @@
+export const concerns = [
+"COR (Certificate of Registration) - BIR",
+"Receipt Need ATP",
+"Books of Accounts",
+"Mayor's Permit",
+"Withholding Tax / 2307",
+"Books",
+"Registration"
+]
